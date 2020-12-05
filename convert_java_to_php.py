ac49a95e31bfda4e23991ec53050ee11f3a3354b
blarom <bar.larom@gmail.com>
2020-12-06 00:15:24 +0200

    Updated java to php converter

--- convert_java_to_php.py
+++ convert_java_to_php.py
@@ -21,45 +21,77 @@ def write_to_file(rwa, filename, content):
 def main():
     java_files = [f for f in listdir(PATH_UTILITIES_CROSS_PLATFORM) if isfile(join(PATH_UTILITIES_CROSS_PLATFORM, f))]
     for java_name in java_files:
+        if 'UtilitiesDb' not in java_name: continue
         content = get_file_contents(f'{PATH_UTILITIES_CROSS_PLATFORM}/{java_name}')
 
-        # Class flattening
-        content = re.sub(r'OverridableUtilitiesGeneral.joinList', 'implode', content)
-        content = re.sub(r'UtilitiesGeneral\.', '', content)
-        content = re.sub(r'Globals\.', '', content)
+        content_new = []
+        last_line = ''
+        for line_pre in content.split('\n'):
 
-        # Line deletions
-        content = re.sub(r'package.+\n', '', content)
-        content = re.sub(r'import.+\n', '', content)
-        content = re.sub(r'\w+ class .+\n', '', content)
-        content = re.sub(r'.+@Contract.+\n', '', content)
-        content = re.sub(r'@NotNull\s+', '', content)
-        content = re.sub(r'.+= new ArrayList.+\n', '', content)
+            line = line_pre
 
-        # Function replacements
-        content = re.sub(r'(public|private) static \S+ ', 'function ', content)
-        content = re.sub(r'\.get\((\d+)\)', '[\g<1>]', content)
-        content = re.sub(r'\.get(\w+)\(\)', lambda match: f'->{match.group(1).lower()}', content)
-        content = re.sub(r'([\w$]+)\.(size\(\)|length\b)', 'sizeof(\g<1>)', content)
-        content = re.sub(r'([^(\s]+).length\(\)', 'mb_strlen(\g<1>)', content)
-        content = re.sub(r'Arrays.asList\(([^)]+)\)', '\g<1>', content)
+            if re.search(r'^(package.+|import.+|\w+ class .+|.+@Contract.+|.+@NotNull\s*|.+= new ArrayList.+)$', line) \
+                    or re.search(r'^\s+(List<[^>]+>|List<List<[^>]+>>|List<List<List<[^>]+>>>|int|boolean|[A-Z][a-z]+)\[*\]*\[*\]*\s+\w+\s*;\s*$', line) \
+                    or re.search(r'^\s+[A-Z]\w+\s+\w+;\s*$', line):
+                continue
 
-        # Variable replacements
-        content = re.sub(r'\s+\w+.+\w+;\s*\n', '', content)
-        content = re.sub(r'for\s*\([\w.]+\s+(\w+)\s*:\s*(\w+)\s*\)', 'foreach ($\g<2> AS $\g<1>)', content)
-        content = re.sub(r'\b(List<[^>]+>|List<List<[^>]+>>|List<List<List<[^>]+>>>|int|boolean|[A-Z][a-z]+)\[*\]*\[*\]*\s+', '', content)
-        content = re.sub(r'else if', 'elseif', content)
-        content = re.sub(r'if\s*\(\s*(\w)', 'if ($\g<1>', content)
-        content = re.sub(r'else\s+(\w)', 'else $\g<1>', content)
-        content = re.sub(r'\)(\s+)([a-z])', ')\g<1>$\g<2>', content)
-        content = re.sub(r'(\|\||&&|return|,|\s+<=*|\s+>=*|\s+=)\s+([a-z])', '\g<1> $\g<2>', content)
-        content = re.sub(r'(\(\s*)([a-z])', '\g<1>$\g<2>', content)
-        content = re.sub(r'(\s*)([a-z]\w*\s*=)', '\g<1>$\g<2>', content)
-        content = re.sub(r'(\(\s*| )\$(\w+)\(', '\g<1>\g<2>(', content)
-        content = re.sub(r'\$\$', '$', content)
+            if re.search(r'^\s*//', line_pre):
+                content_new.append(line)
+                continue
+
+
+            # Class flattening
+            line = re.sub(r'OverridableUtilitiesGeneral.joinList', 'implode', line)
+            line = re.sub(r'OverridableUtilitiesDb.', '', line)
+            line = re.sub(r'UtilitiesGeneral\.', '', line)
+            line = re.sub(r'Globals\.', '', line)
+
+            # Function replacements
+            line = re.sub(r'(public|private) static \S+ ', 'function ', line)
+            line = re.sub(r'\.get\((\d+)\)', '[\g<1>]', line)
+            line = re.sub(r'\.get(\w+)\(\)', lambda match: f'->{match.group(1).lower()}', line)
+            line = re.sub(r'\.(is\w+)\(\)', f'->\g<1>', line)
+            line = re.sub(r'([^,;\s]+).length\(\)', 'mb_strlen(\g<1>)', line)
+            line = re.sub(r'([\w$]+)\.(size\(\)|length\b)', 'sizeof(\g<1>)', line)
+            line = re.sub(r'Arrays.asList\(([^)]+)\)', '\g<1>', line)
+            line = re.sub(r'\(([^,;\s]+)\.(addAll|add)\((.+)\)', '(array_push(\g<1>, \g<3>)', line)
+            line = re.sub(r'\(([^,;\s]+)\.subList\(([^,;]+),([^,;]+)\)', '(array_slice(\g<1>, \g<2>, \g<3>)', line)
+            line = re.sub(r'\(([^,;\s]+)\.substring\(([^,;]+),([^,;]+)\)', '(mb_substr(\g<1>, \g<2>, \g<3>)', line)
+            line = re.sub(r'\(([^,;\s]+).equals\(([^,;\s]+)\)\)', '(\g<1> == \g<2>', line)
+            line = re.sub(r'\(([^,;\s]+).contains\(([^,;\s]+)\)', '(contains(\g<1>, \g<2>)', line)
+            line = re.sub(r'\(([^,;\s]+).trim\(\)', '(trim(\g<1>)', line)
+            line = re.sub(r'([^,;\s]+)\.(addAll|add)\((.+)\)', 'array_push(\g<1>, \g<3>)', line)
+            line = re.sub(r'([^,;\s]+)\.subList\(([^,;]+),([^,;]+)\)', 'array_slice(\g<1>, \g<2>, \g<3>)', line)
+            line = re.sub(r'([^,;\s]+)\.substring\(([^,;]+),([^,;]+)\)', 'mb_substr(\g<1>, \g<2>, \g<3>)', line)
+            line = re.sub(r'([^,;\s]+).equals\(([^,;\s]+)\)', '\g<1> == \g<2>', line)
+            line = re.sub(r'([^,;\s]+).contains\(([^,;\s]+)\)', 'contains(\g<1>, \g<2>)', line)
+            line = re.sub(r'([^,;\s]+).trim\(\)', 'trim(\g<1>)', line)
+
+            # Variable replacements
+            line = re.sub(r'@NotNull\s+', '', line)
+            line = re.sub(r'for\s*\([\w.]+\s+(\w+)\s*:\s*(\w+)\s*\)', 'foreach ($\g<2> AS $\g<1>)', line)
+            line = re.sub(r'\b(List<[^>]+>|List<List<[^>]+>>|List<List<List<[^>]+>>>|int|boolean|[A-Z][a-z]\w+)\[*\]*\[*\]*\s+', '', line)
+            line = re.sub(r'else if', 'elseif', line)
+            line = re.sub(r'if\s*\(\s*(\w)', 'if ($\g<1>', line)
+            line = re.sub(r'else\s+(\w)', 'else $\g<1>', line)
+            line = re.sub(r'\)(\s+)([a-z])', ')\g<1>$\g<2>', line)
+
+            if not re.search(r'^\s*(function\s+|if\s+|else\s+|elseif\s+|return\s+|break\s*;|for\s*\(|foreach\s*\()', line):
+                line = re.sub(r'^(\s*)([a-z])', '\g<1>$\g<2>', line)
+
+            line = re.sub(r'(\|\||&&|return|,|\s+<=*|\s+>=*|\s+==|\s+=)\s+([a-z])', '\g<1> $\g<2>', line)
+            line = re.sub(r'(\(\s*)([a-z])', '\g<1>$\g<2>', line)
+            line = re.sub(r'(\s*)([a-z]\w*\s*=)', '\g<1>$\g<2>', line)
+            line = re.sub(r'(\(\s*| )\$(\w+)\(', '\g<1>\g<2>(', line)
+            line = re.sub(r'\$\$', '$', line)
+            line = re.sub(r'!(\w)', '!$\g<1>', line)
+            line = re.sub(r'\$null\b', 'null', line)
+
+            if line or last_line: content_new.append(line)
+            last_line = line
 
         php_name = java_name + '.php'
-        write_to_file('w+', f'{PATH_PHP}/{php_name}', '<?php\n' + content + '\n?>')
+        write_to_file('w+', f'{PATH_PHP}/{php_name}', '<?php\n' + '\n'.join(content_new) + '\n?>')
 
 
 main()
